aca49dcd77a7fa9e8dbb0b9a6cd10f797b18e546
Lyle Troxell <lyle@troxell.com>
2013-05-19 11:53:52 -0700

    Initial commit

---
+++ sj/Model.js
@@ -0,0 +1,21 @@
+﻿
+guidedModel =// @startlock
+{
+	Visitors :
+	{
+		email :
+		{
+			events :
+			{
+				onLoad:function(attributeName)
+				{// @endlock
+					try {
+						currentSession().checkPermission('admin');
+					}catch(e){
+						this.email = "****";
+					}
+				}// @startlock
+			}
+		}
+	}
+};// @endlock
---
+++ sj/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,36 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var createVisitor = {};	// @button
+	var saveVisitors = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	createVisitor.click = function createVisitor_click (event)// @startlock
+	{// @endlock
+				$('#visitorForm').show();
+
+	};// @lock
+
+	saveVisitors.click = function saveVisitors_click (event)// @startlock
+	{// @endlock
+		$('#saveVisitor').focus();
+		waf.sources.visitors.save({
+			onSuccess: function(event){
+				alert('Thanks '+ sources.visitors.firstName);
+				$('#visitorForm').hide();
+			},
+			onError: function(event){
+				alert('oh, drat '+event.error[0].message);
+			}
+		});
+	
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("createVisitor", "click", createVisitor.click, "WAF");
+	WAF.addListener("saveVisitors", "click", saveVisitors.click, "WAF");
+// @endregion
+};// @endlock
